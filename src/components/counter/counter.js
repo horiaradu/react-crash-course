374ae94d8a6a76b9957ef17436d95a9e956e370b
Horia Radu <horia.radu23@gmail.com>
2018-12-24 09:28:30 +0200

    useEffect optimisation

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,20 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Counter = props => {
   const [count, setCount] = useState(0);
 
   const increment = () => setCount(count + 1);
 
+  const [title, setTitle] = useState(`Counter (${props.name}): ${count}`);
+  useEffect(() => setTitle(`Counter (${props.name}): ${count}`), [count]);
+
   return (
     <p>
       <button onClick={increment}>click</button>
       <br />
       <br />
-      <span>
-        Counter ({props.name}): {count}
-      </span>
+      <span>{title}</span>
     </p>
   );
 };
